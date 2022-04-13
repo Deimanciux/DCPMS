5f7c3eb3030fc64ccd33e395ec48b74bf8902fd1
Deimante <deimante.beniusyte@nfq.lt>
2022-04-13 10:31:25 +0300

    FR-3 images added for services and fullcalendar downloaded

---
+++ app/src/Entity/ServiceImage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ServiceImageRepository::class)]
+class ServiceImage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\ManyToOne(targetEntity: Service::class, inversedBy: 'serviceImages')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $service;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $path;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getService(): ?Service
+    {
+        return $this->service;
+    }
+
+    public function setService(?Service $service): self
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+}
